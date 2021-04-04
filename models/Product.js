2ec6147e990bfbf08e8156275ffcf53a3efe4846
iigonzoii <a.gonzalez418@gmail.com>
2021-04-04 08:07:52 -0600

    added product column in models

--- models/Product.js
+++ models/Product.js
@@ -7,9 +7,41 @@ const sequelize = require('../config/connection');
 class Product extends Model {}
 
 // set up fields and rules for Product model
+// activity 28 for reference
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+        type: DataTypes.DECIMAL,
+        validate:{
+        isDecimal: true
+        },
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isInt: true,
+      },
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id',
+      },
+    }
   },
   {
     sequelize,
