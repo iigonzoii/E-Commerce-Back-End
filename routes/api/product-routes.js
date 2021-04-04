617510dd6d2a65e53db50021deaa3f24d8bfb9cd
iigonzoii <a.gonzalez418@gmail.com>
2021-04-04 17:04:52 -0600

    get all products route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,6 +7,17 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const allProducts = await Product.findAll({
+      include: [
+        {model: Category},
+        {model: Tag, through: ProductTag, as: 'Product_tags'}
+      ],
+    });
+    res.status(200).json(allProducts);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
