f233d431e83c83b715670e3ef64c737212d7d06b
iigonzoii <a.gonzalez418@gmail.com>
2021-04-05 11:43:46 -0600

    .get for category-routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -2,10 +2,19 @@ const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
-
-router.get('/', (req, res) => {
+// activity 28 to the rescue again
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    // here we use the constructor class of category in our models and find all of its associated products by including them using deconstructing? im pretty sure thats whats happening here
+    const allCategories = await Category.findAll({
+      include: [{model: Product}]
+    });
+    res.status(200).json(allCategories);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
